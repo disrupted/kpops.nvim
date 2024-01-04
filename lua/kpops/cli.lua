1146bcb2609223db62e665560c34520420bd0753
Salomon Popp <salomon.popp@bakdata.com>
2024-01-04 10:31:26 +0100

    Remove components module from schema command
    automatically detected through config.yaml in KPOps v3

--- lua/kpops/cli.lua
+++ lua/kpops/cli.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-M.schema = function(scope, module)
-  local result = vim.system({ 'kpops', 'schema', scope, module }):wait()
+M.schema = function(scope)
+  local result = vim.system({ 'kpops', 'schema', scope }):wait()
   if result.code ~= 0 then
     vim.notify(
       ('KPOps error generating pipeline schema: %s'):format(result.stderr),

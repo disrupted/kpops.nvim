730c8fbe3cb627fa7738f8682762765873369709
Salomon Popp <salomon.popp@bakdata.com>
2024-01-25 22:40:02 +0100

    style: format

--- lua/kpops/init.lua
+++ lua/kpops/init.lua
@@ -1,5 +1,5 @@
 local lsp = require('kpops.lsp')
-local commands=require('kpops.commands')
+local commands = require('kpops.commands')
 
 local M = {}
 
@@ -32,7 +32,7 @@ local conf = {
 
 -- -@param opts table<string, any>
 M.setup = function(opts)
-    commands.setup()
+  commands.setup()
   conf = vim.tbl_deep_extend('force', conf, opts or {})
   lsp.setup(conf)
 end

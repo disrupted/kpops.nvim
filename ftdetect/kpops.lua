3c1fa7b8c1235cc7cbe667762396f4f765c64d85
Salomon Popp <salomon.popp@bakdata.com>
2025-01-14 10:12:20 +0100

    fix(ftdetect): should not attach to `configmap.yaml`

--- ftdetect/kpops.lua
+++ ftdetect/kpops.lua
@@ -2,9 +2,14 @@ if not vim.fn.executable('kpops') == 1 then
   return
 end
 vim.filetype.add({
+  filename = {
+    ['pipeline.yaml'] = 'yaml.kpops',
+    ['defaults.yaml'] = 'yaml.kpops',
+    ['config.yaml'] = 'yaml.kpops',
+  },
   pattern = {
-    ['pipeline[_%w]*%.yaml'] = 'yaml.kpops',
-    ['defaults[_%w]*%.yaml'] = 'yaml.kpops',
-    ['config[_%w]*%.yaml'] = 'yaml.kpops',
+    ['pipeline_[%w]*%.yaml'] = 'yaml.kpops',
+    ['defaults_[%w]*%.yaml'] = 'yaml.kpops',
+    ['config_[%w]*%.yaml'] = 'yaml.kpops',
   },
 })
